d7578e1939101f794994f97f5d2e1cff3df400d8
Mark Struck <markstruck@comcast.net>
2016-07-10 04:04:06 -0500

    Initial check-in

---
+++ Component.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    _handleClick: function(event) {
+        alert();
+    },
+
+    render: function() {
+        return (
+            <html>
+                <head>
+                    <title>Universal App with React</title>
+                    <link rel='stylesheet' href='/style.css' />
+                </head>
+                <body>
+                    <div>
+                        <h1>Hello World!</h1>
+                        <p>Isn't server-side rendering remarkable?</p>
+                        <button onClick={this._handleClick}>Click Me</button>
+                    </div>
+                    <script src='/bundle.js' />
+                </body>
+            </html>
+        )
+    }
+});
---
+++ server.js
@@ -0,0 +1,26 @@
+require('babel-register')({
+    presets: ['react']
+});
+
+const express = require('express');
+const app = express();
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Component = require('./Component.jsx');
+
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+    var html = ReactDOMServer.renderToString(
+        React.createElement(Component)
+    );
+
+    response.send(html);
+});
+
+var PORT = 3000;
+
+app.listen(PORT, function() {
+    console.log('http://localhost:3000');
+});
